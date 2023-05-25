0a49f926a43d8dd42c6fe051b2a48b847832bca2
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2023-05-24 18:08:37 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS, cross_origin
 import json
+import os
 
 app = Flask(__name__, static_folder="./resources/")
 CORS(app)
 
-@app.route("/", methods=["GET", "POST"])
+
+
+@app.route('/', methods=['GET', 'POST'])
 @cross_origin(origin="*")
 def home():
+    if request.method == 'POST':
+        return redirect(url_for('task'))
+    return render_template('home.html')
+
+@app.route("/task", methods=["GET", "POST"])
+@cross_origin(origin="*")
+def task():
 	if (request.method == "POST"):
 		form = request.form
 		x = form.to_dict(flat=False)
-		with open(form['name'].lower()+'.json','w') as f:
-			f.write(json.dumps(x,indent=4))
-		return render_template('story.html')
+		path = os.getcwd()+ "/"+form['name'].lower()+'.json'
+		if os.path.isfile(path)==False:
+			with open(form['name'].lower()+'.json','w') as f:
+				f.write(json.dumps(x,indent=4))
+			return render_template('story.html')
+		else:
+			with open(form['name'].lower()+'_updated.json','w') as f:
+				f.write(json.dumps(x,indent=4))
+			return render_template('index.html')
 	return render_template('index.html')
+
 
 
 if __name__ == '__main__':
